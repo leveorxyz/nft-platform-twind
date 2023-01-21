7e5f1c160f6e8e64539a434bd2a784bc7dce80bc
Mahmudul Alam <expelmahmud@gmail.com>
2023-01-21 09:42:48 +0600

    Added logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+  return (
+    <div className="text-3xl font-bold bg-clip-text uppercase text-bold logo-gradient tracking-wider">
+      NFT.MARKET
+    </div>
+  );
+};
+
+Logo.defaultProps = {
+  fontSize: '32px',
+};
+
+export default Logo;
