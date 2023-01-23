868432a9a7339895f9f3bc08607d60e7c0dfd241
Mahmudul Alam <expelmahmud@gmail.com>
2023-01-23 15:01:31 +0600

    Added explore tabs

---
+++ src/components/collections/ExploreTabs.tsx
@@ -0,0 +1,36 @@
+import { Tab } from '@headlessui/react';
+
+const tabs = [
+  'All NFT',
+  'sports',
+  'music',
+  'art',
+  'collectibles',
+  'trading cards',
+  'utility',
+  'photography',
+];
+
+const ExploreTabs = () => {
+  return (
+    <Tab.Group>
+      <Tab.List className="space-x-4">
+        {tabs.map((tab) => (
+          <Tab key={tab}>
+            {({ selected }) => (
+              <button
+                className={`px-3 py-2 font-medium text-sm rounded-md cursor-pointer capitalize hover:bg-accent  ${
+                  selected ? 'bg-accent text-indigo-700' : 'text-gray-500 hover:text-gray-700'
+                }`}
+              >
+                {tab}
+              </button>
+            )}
+          </Tab>
+        ))}
+      </Tab.List>
+    </Tab.Group>
+  );
+};
+
+export default ExploreTabs;
