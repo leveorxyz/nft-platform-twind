ca386771a0ad440b33bf7a1e2415a3e7681c2da8
Mahmudul Alam <expelmahmud@gmail.com>
2023-01-26 16:17:53 +0600

    Added offer tab

--- src/components/nft/NFTTabs.tsx
+++ src/components/nft/NFTTabs.tsx
@@ -1,70 +1,50 @@
-import {
-  IoList,
-  IoOptions,
-  IoNewspaper,
-  IoSwapVerticalOutline,
-  IoTrendingUp,
-} from 'react-icons/io5';
+import { Tab } from '@headlessui/react';
+import Offer from './tabs/Offer';
 
 const tabs = [
-  { name: 'Offers', href: '#', icon: IoList, current: false },
-  { name: 'Properties', href: '#', icon: IoOptions, current: false },
-  { name: 'Details', href: '#', icon: IoNewspaper, current: true },
-  { name: 'Activities', href: '#', icon: IoSwapVerticalOutline, current: false },
-  { name: 'Price History', href: '#', icon: IoTrendingUp, current: false },
+  { name: 'Offers', icon: 'offers', panel: Offer },
+  { name: 'Properties', icon: 'properties', panel: Offer },
+  { name: 'Details', icon: 'details', panel: Offer },
+  { name: 'Activities', icon: 'activity', panel: Offer },
+  { name: 'Price History', icon: 'price', panel: Offer },
 ];
 
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(' ');
-}
-
-export default function NFTTabs() {
+const NFTTabs = () => {
   return (
     <div>
-      <div className="sm:hidden">
-        <label htmlFor="tabs" className="sr-only">
-          Select a tab
-        </label>
-        {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
-        <select
-          id="tabs"
-          name="tabs"
-          className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
-          // defaultValue={tabs.find((tab) => tab.current).name}
-        >
+      <Tab.Group>
+        <Tab.List className="space-x-4">
           {tabs.map((tab) => (
-            <option key={tab.name}>{tab.name}</option>
+            <Tab key={tab.name}>
+              {({ selected }) => (
+                <button
+                  className={`px-3 py-2 font-medium text-md cursor-pointer capitalize ${
+                    selected
+                      ? 'border-b-2 border-accent text-indigo-700'
+                      : 'text-brand-300 hover:text-brand-700 dark:hover:text-white'
+                  }`}
+                >
+                  <span className="flex items-center gap-2">
+                    <svg className="icon mr-1 h-5 w-5 fill-current">
+                      <use xlinkHref={`/images/icons/icons.svg#icon-${tab.icon}`}></use>
+                    </svg>{' '}
+                    {tab.name}
+                  </span>
+                </button>
+              )}
+            </Tab>
           ))}
-        </select>
-      </div>
-      <div className="hidden sm:block">
-        <div className="border-gray-200">
-          <nav className="-mb-px flex space-x-8" aria-label="Tabs">
-            {tabs.map((tab) => (
-              <a
-                key={tab.name}
-                href={tab.href}
-                className={classNames(
-                  tab.current
-                    ? 'border-indigo-500 text-indigo-600'
-                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                  'group inline-flex items-center py-4 px-1 border-b-2 font-medium text-sm'
-                )}
-                aria-current={tab.current ? 'page' : undefined}
-              >
-                <tab.icon
-                  className={classNames(
-                    tab.current ? 'text-indigo-500' : 'text-gray-400 group-hover:text-gray-500',
-                    '-ml-0.5 mr-2 h-5 w-5'
-                  )}
-                  aria-hidden="true"
-                />
-                <span>{tab.name}</span>
-              </a>
-            ))}
-          </nav>
-        </div>
-      </div>
+        </Tab.List>
+        <Tab.Panels className="mt-1">
+          {tabs.map((tab) => (
+            <Tab.Panel key={tab.name} id={tab.name}>
+              <tab.panel />
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
-}
+};
+
+export default NFTTabs;
---
+++ src/components/nft/tabs/Offer.tsx
@@ -0,0 +1,48 @@
+import offers from '@/data/offers.json';
+
+const Offer = () => {
+  return (
+    <table className="min-w-full divide-y divide-brand-100 dark:bg-brand-700  dark:border-brand-600 dark:divide-brand-600">
+      <thead className="bg-light-base dark:bg-brand-600 dark:border-brand-60">
+        <tr>
+          {labels.map((label) => (
+            <th
+              key={label}
+              scope="col"
+              className="py-3 px-3 text-center text-sm font-semibold text-gray-900 sm:pl-6"
+            >
+              {label}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody className="divide-y divide-brand-100 dark:bg-brand-700 dark:divide-brand-600">
+        {offers.map((item) => (
+          <tr key={item.id}>
+            <td className="whitespace-nowrap text-center py-3 text-sm font-medium text-gray-900 sm:pl-6">
+              {item.price}
+            </td>
+            <td className="whitespace-nowrap text-center md:px-6 py-3 text-sm text-gray-500">
+              {item.usdPrice}
+            </td>
+            <td className="whitespace-nowrap text-center md:px-6 py-3 text-sm text-gray-500">
+              {item.floorDifference}
+            </td>
+            <td className="whitespace-nowrap text-center md:px-6 py-3 text-sm text-gray-500">
+              {item.expiration}
+            </td>
+            <td className="whitespace-nowrap text-center md:px-6 py-3 text-sm text-gray-500">
+              <a href="#!" className="text-accent hover:text-accent-dark">
+                {item.from}
+              </a>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Offer;
+
+const labels = ['Price (ETH)', 'USD Price', 'Floor Difference', 'Expiration', 'From'];
