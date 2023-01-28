33cd86872554b81a746cb61aa125446753b2f5cc
Mahmudul Alam <expelmahmud@gmail.com>
2023-01-28 22:38:19 +0600

    Added rainbowkit

--- src/components/headers/HeaderOne.tsx
+++ src/components/headers/HeaderOne.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { Menu, Transition } from '@headlessui/react';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 import DarkMode from '../darkmode/DarkMode';
 import Logo from '../Logo/Logo';
 import SearchForm from './SearchForm';
@@ -62,22 +62,14 @@ export default function Header01() {
                     </button>
                   </Link>
                 </li>
+                {/* <li className="group py-0.5">
+                  <ConnectButton />
+                </li> */}
               </ul>
             </nav>
 
-            <div className="ml-8 hidden items-center lg:flex xl:ml-12">
-              <button className="border-brand-100 hover:bg-accent focus:bg-accent group dark:hover:bg-accent flex h-10 w-10 items-center justify-center rounded-full border bg-white transition-colors hover:border-transparent focus:border-transparent dark:border-transparent dark:bg-white/[.15]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width={24}
-                  height={24}
-                  className="h-4 w-4 fill-brand-700 transition-colors group-hover:fill-white group-focus:fill-white dark:fill-white"
-                >
-                  <path fill="none" d="M0 0h24v24H0z" />
-                  <path d="M22 6h-7a6 6 0 1 0 0 12h7v2a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2zm-7 2h8v8h-8a4 4 0 1 1 0-8zm0 3v2h3v-2h-3z" />
-                </svg>
-              </button>
+            <div className="ml-8 hidden items-center lg:flex xl:ml-12 w-full">
+              <ConnectButton />
               <AccountMenu />
               <DarkMode />
             </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,37 @@
 import '@/styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY as string }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'NFT Cart',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </ThemeProvider>
   );
 }
