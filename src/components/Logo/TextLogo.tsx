cac76566489e5a93726656b4b17d039966e8b5b4
Mahmudul Alam <expelmahmud@gmail.com>
2023-01-29 13:27:46 +0600

    Updated logo

---
+++ src/components/Logo/TextLogo.tsx
@@ -0,0 +1,9 @@
+const TextLogo = () => {
+  return (
+    <div className="text-xl font-bold bg-clip-text uppercase text-bold logo-gradient tracking-wider md:text-3xl">
+      NFT.CART
+    </div>
+  );
+};
+
+export default TextLogo;
